48663c1b2c640a19b74a8bedc99d9060f08ae258
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-10-12 06:55:59 +0900

    react iconを導入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import CoverImage from "../images/mydesk.png";
 import ProfileImage from "../images/profile-image.png";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
 export const Header = () => {
   const componentName = () => "Header";
@@ -20,8 +21,16 @@ export const Header = () => {
             <h1 className="title-text">藤江　正樹</h1>
             <h3 className="title-text">Webエンジニア</h3>
             <ul className="social-icons">
-              <li className="icon-link">todo</li>
-              <li className="icon-link">todo</li>
+              <li className="icon-link">
+                <a href="https://twitter.com/Maski23421424">
+                  <FaTwitter color="white" size="2rem"></FaTwitter>
+                </a>
+              </li>
+              <li className="icon-link">
+                <a href="https://github.com/FujieMasaki">
+                  <FaGithub color="white" size="2rem"></FaGithub>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
