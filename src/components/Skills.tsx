fb8242c41c6f673d2ec9cd8b029499b32507fa27
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-10-13 22:42:19 +0900

    useEffectでgithub APIのデータを取得

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const Skills = () => {
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/FujieMasaki/repos")
+      .then((response) => console.log(response));
+  }, []);
+
   return (
     <div id="skills">
       <div className="container">
