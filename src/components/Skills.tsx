486520c1ed51acdeb3262ccf4712e67ef4590e35
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-10-16 22:23:35 +0900

    github_repositoryから言語を取得

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,49 @@
 import React from "react";
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
+import { count } from "console";
+
+type dataType = {
+  name: string;
+  full_name: string;
+};
+
+type Language = {
+  language: string;
+  count: number;
+  private?: boolean;
+};
 
 export const Skills = () => {
+  const [languageList, setLanguageList] = useState([]);
+  console.log(languageList);
   useEffect(() => {
     axios
       .get("https://api.github.com/users/FujieMasaki/repos")
-      .then((response) => console.log(response));
+      .then((response) => {
+        const languageList = response.data.map(
+          (res: { language: string }) => res.language
+        );
+        // ['JavaScript', 'JavaScript', 'Ruby', null]な、かたちで返される
+        const countedLanguageList =
+          generateLanguageCountObj(languageList);
+        setLanguageList(countedLanguageList);
+      });
   }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(
+      (language) => language != null
+    );
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map((item) => {
+      return {
+        language: item,
+        count: allLanguageList.filter((language) => language === item).length,
+      };
+    });
+  };
 
   return (
     <div id="skills">
